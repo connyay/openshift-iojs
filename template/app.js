e16de9cd5d9b59d8b00a41db4f3d0318dc5082f4
Peter Robins <github@peterrobins.co.uk>
2015-06-21 20:29:59 +0100

    Use ES6 in app.js

--- template/app.js
+++ template/app.js
@@ -1,13 +1,16 @@
 'use strict';
-var http = require('http');
-var env = process.env;
-var port = env.OPENSHIFT_IOJS_PORT || 1337;
-var ip = env.OPENSHIFT_IOJS_IP || '127.0.0.1';
+let http = require('http');
+let env = process.env;
+let port = env.OPENSHIFT_IOJS_PORT || 1337;
+let ip = env.OPENSHIFT_IOJS_IP || '127.0.0.1';
 
 http.createServer(function(req, res) {
-    var body = 'Welcome to io.js on OpenShift!\n\n' +
-        'Everything seems to be in order.\n' +
-        'Running io.js ' + process.version;
+    let body = `Welcome to io.js on OpenShift!
+  Everything seems to be in order.
+  Running io.js`;
+    for (let v in process.versions) {
+      body += `\n- ${v}: ${process.versions[v]}`;
+    }
 
     res.writeHead(200, {
         'Content-Type': 'text/plain'
