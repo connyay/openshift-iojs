cdbb8a621b96d96ccd7378af1b3fee25a3edcb59
Connor Hindley <connor@redhat.com>
2015-08-05 08:26:28 -0400

    bump to 3.0.0

--- bin/iojs/include/node/node_buffer.h
+++ bin/iojs/include/node/node_buffer.h
@@ -2,7 +2,6 @@
 #define SRC_NODE_BUFFER_H_
 
 #include "node.h"
-#include "smalloc.h"
 #include "v8.h"
 
 #if defined(NODE_WANT_INTERNALS)
@@ -12,7 +11,10 @@
 namespace node {
 namespace Buffer {
 
-static const unsigned int kMaxLength = smalloc::kMaxLength;
+static const unsigned int kMaxLength =
+    sizeof(int32_t) == sizeof(intptr_t) ? 0x3fffffff : 0x7fffffff;
+
+NODE_EXTERN typedef void (*FreeCallback)(char* data, void* hint);
 
 NODE_EXTERN bool HasInstance(v8::Handle<v8::Value> val);
 NODE_EXTERN bool HasInstance(v8::Handle<v8::Object> val);
@@ -21,53 +23,30 @@ NODE_EXTERN char* Data(v8::Handle<v8::Object> val);
 NODE_EXTERN size_t Length(v8::Handle<v8::Value> val);
 NODE_EXTERN size_t Length(v8::Handle<v8::Object> val);
 
-// public constructor
-NODE_EXTERN v8::Local<v8::Object> New(v8::Isolate* isolate, size_t length);
-NODE_DEPRECATED("Use New(isolate, ...)",
-                inline v8::Local<v8::Object> New(size_t length) {
-  return New(v8::Isolate::GetCurrent(), length);
-})
-// public constructor from string
-NODE_EXTERN v8::Local<v8::Object> New(v8::Isolate* isolate,
-                                      v8::Handle<v8::String> string,
-                                      enum encoding enc = UTF8);
-NODE_DEPRECATED("Use New(isolate, ...)",
-                inline v8::Local<v8::Object> New(v8::Handle<v8::String> string,
-                                                 enum encoding enc = UTF8) {
-  return New(v8::Isolate::GetCurrent(), string, enc);
-})
 // public constructor - data is copied
-// TODO(trevnorris): should be something like Copy()
-NODE_EXTERN v8::Local<v8::Object> New(v8::Isolate* isolate,
-                                      const char* data,
-                                      size_t len);
-NODE_DEPRECATED("Use New(isolate, ...)",
-                inline v8::Local<v8::Object> New(const char* data, size_t len) {
-  return New(v8::Isolate::GetCurrent(), data, len);
-})
+NODE_EXTERN v8::MaybeLocal<v8::Object> Copy(v8::Isolate* isolate,
+                                            const char* data,
+                                            size_t len);
+
+// public constructor
+NODE_EXTERN v8::MaybeLocal<v8::Object> New(v8::Isolate* isolate, size_t length);
+
+// public constructor from string
+NODE_EXTERN v8::MaybeLocal<v8::Object> New(v8::Isolate* isolate,
+                                           v8::Handle<v8::String> string,
+                                           enum encoding enc = UTF8);
+
 // public constructor - data is used, callback is passed data on object gc
-NODE_EXTERN v8::Local<v8::Object> New(v8::Isolate* isolate,
-                                      char* data,
-                                      size_t length,
-                                      smalloc::FreeCallback callback,
-                                      void* hint);
-NODE_DEPRECATED("Use New(isolate, ...)",
-                inline v8::Local<v8::Object> New(char* data,
-                                                 size_t length,
-                                                 smalloc::FreeCallback callback,
-                                                 void* hint) {
-  return New(v8::Isolate::GetCurrent(), data, length, callback, hint);
-})
+NODE_EXTERN v8::MaybeLocal<v8::Object> New(v8::Isolate* isolate,
+                                           char* data,
+                                           size_t length,
+                                           FreeCallback callback,
+                                           void* hint);
 
 // public constructor - data is used.
-// TODO(trevnorris): should be New() for consistency
-NODE_EXTERN v8::Local<v8::Object> Use(v8::Isolate* isolate,
-                                      char* data,
-                                      uint32_t len);
-NODE_DEPRECATED("Use Use(isolate, ...)",
-                inline v8::Local<v8::Object> Use(char* data, uint32_t len) {
-  return Use(v8::Isolate::GetCurrent(), data, len);
-})
+NODE_EXTERN v8::MaybeLocal<v8::Object> New(v8::Isolate* isolate,
+                                           char* data,
+                                           size_t len);
 
 // This is verbose to be explicit with inline commenting
 static inline bool IsWithinBounds(size_t off, size_t len, size_t max) {
@@ -84,18 +63,17 @@ static inline bool IsWithinBounds(size_t off, size_t len, size_t max) {
   return true;
 }
 
-// Internal. Not for public consumption. We can't define these in
-// src/node_internals.h due to a circular dependency issue with
-// the smalloc.h and node_internals.h headers.
+// Internal. Not for public consumption. We can't define these
+// in src/node_internals.h because of a circular dependency.
 #if defined(NODE_WANT_INTERNALS)
-v8::Local<v8::Object> New(Environment* env, size_t size);
-v8::Local<v8::Object> New(Environment* env, const char* data, size_t len);
-v8::Local<v8::Object> New(Environment* env,
-                          char* data,
-                          size_t length,
-                          smalloc::FreeCallback callback,
-                          void* hint);
-v8::Local<v8::Object> Use(Environment* env, char* data, uint32_t length);
+v8::MaybeLocal<v8::Object> New(Environment* env, size_t size);
+v8::MaybeLocal<v8::Object> New(Environment* env, const char* data, size_t len);
+v8::MaybeLocal<v8::Object> New(Environment* env,
+                               char* data,
+                               size_t length,
+                               FreeCallback callback,
+                               void* hint);
+v8::MaybeLocal<v8::Object> Use(Environment* env, char* data, size_t length);
 #endif  // defined(NODE_WANT_INTERNALS)
 
 }  // namespace Buffer
